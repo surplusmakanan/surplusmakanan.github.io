e8200c4e6d8842c776730394016f3c6a82acfb78
surplusmakanan <surplusmakanan@gmail.com>
2020-11-17 19:26:14 +0700

    WebGIS

---
+++ Icosaster/dapur/data/DapurUmum_4.js
@@ -0,0 +1,18 @@
+var json_DapurUmum_4 = {
+"type": "FeatureCollection",
+"name": "DapurUmum_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama_Dapur": "Kodam V Brawijaya", "Alamat": "Jl. Raden Wijaya No.1, Sawunggaling, Kec. Wonokromo, Kota SBY, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.720842687004662, -7.299271058078109 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Balaikota Surabaya", "Alamat": "Jl. Walikota Mustajab No.59, Ketabang, Kec. Genteng, Kota SBY, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.744848611767836, -7.259295687168777 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Pondok Maritim", "Alamat": "Jl. Sulawesi No. 5 Gubeng, Surabaya" }, "geometry": { "type": "Point", "coordinates": [ 112.742489954105494, -7.276945266463718 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Wiratno", "Alamat": "Jl. Wiratno No. 4, Kenjeran , Kec. Bulak, Kota SBY, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.79033582322964, -7.251541738473945 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Bratang Binangun", "Alamat": "Jl. Bratang Binangun II No. 43, RW.8, Baratajaya, Kec. Gubeng, Kota Suarabaya, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.757334689783164, -7.294085232546442 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Balai Achmad Jais", "Alamat": "Jl. Achmad Jais, Paneleh, Kec. Genteng, Kota Surabaya, Jawa Timu" }, "geometry": { "type": "Point", "coordinates": [ 112.737680987278793, -7.255561788447379 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Sedayu", "Alamat": "Jl. Sedayu No. 22-24, Morokrembangan, Kec. Krembangan, Kota Surabaya, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.721835004009591, -7.236453338219142 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Jemur Wonosari", "Alamat": "Gg. Lebar Jl. Jemur Wonosari No. 97 RW. 9, Jemur Wonosari, Kec. Wonocolo, Kota Surabaya, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.734239724535087, -7.323695968907055 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Kemayoran Budidayan", "Alamat": "Gang I Jl Kemayoran-Budidayan No. 14, Kemayora,, Kec. Krembangan, Kota Surabaya, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.666208872050916, -7.323690765743918 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Gresik Gadukan", "Alamat": "Jl. Gresik Gadukan Timur No. 99, Morokrembangan, Kec. Krembangan, Kota Surabaya, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.718261437324955, -7.232443188055765 ] } },
+{ "type": "Feature", "properties": { "Nama_Dapur": "Nginden", "Alamat": "Jl. Nginden Kota III No. 29, Baratajaya, Kec. Gubeng, Kota Surabaya, Jawa Timur" }, "geometry": { "type": "Point", "coordinates": [ 112.758327862125682, -7.300293015388374 ] } }
+]
+}
